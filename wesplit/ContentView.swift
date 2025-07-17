f2fae7565e53b03703eee271ed5c00f3082c5ed6
Mohsin-khan540 <arhamk4555@gmail.com>
2025-07-18 00:11:51 +0500

    initial commit

--- wesplit/ContentView.swift
+++ wesplit/ContentView.swift
@@ -8,17 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkBalance = 0.0
+    @State private var NoOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+//    let tipPercentages = [15, 20, 25, 0]
+    // computed property
+    var totalPerPerson: Double{
+        let peopleCount = Double(NoOfPeople + 2)
+        let tipPercentage = Double(tipPercentage)
+        let tipValue = checkBalance/100 * tipPercentage
+        let grandTotal = checkBalance + tipValue
+        let amountperPerson = grandTotal/peopleCount
+        return amountperPerson
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+        Form{
+           
+                Section{
+                    TextField("Check Balance", value: $checkBalance,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $NoOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) peoples")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+            Section("How much tip do you want to give?"){
+                Picker("tip percentage" , selection : $tipPercentage){
+                    ForEach(0..<101){
+                        Text("\($0)% tip")
+                    }
+                }
+                .pickerStyle(.navigationLink)
+//                .pickerStyle(.segmented)
+            }
+            Section("Total amount  without tip"){
+                Text(checkBalance, format: .currency(code: "PKR"))
+            }
+           
+                Section("Amount per person"){
+                    Text(totalPerPerson,format: .currency(code: Locale.current.currency? .identifier ?? "USD") .precision(.fractionLength(2)))
+                    
+                }
+            }
+        .navigationTitle("Welcome to the cage cafe")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar{
+            if amountIsFocused{
+                Button("Done"){
+                    amountIsFocused = false
+                }
+            }
         }
-        .padding()
+            Section{
+                Button("Reset"){
+                    checkBalance=0
+                    NoOfPeople=0
+                    tipPercentage=20
+                }
+                .foregroundColor(.red)
+            }
+            
+        }
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
