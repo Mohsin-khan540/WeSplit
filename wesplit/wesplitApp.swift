3158c65e9d0c8991b091d0f3568d9c2694b33111
Mohsin-khan540 <arhamk4555@gmail.com>
2025-07-17 19:17:33 +0500

    Initial Commit

---
+++ wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by Mohsin khan on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
